1e800a7c596db74eda769bcd670d356e880b44e6
irnes-duran <irnes.duran@waltercode.com>
2023-02-01 16:12:20 +0100

    3d done

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,28 @@ import './styles.css'
 import SceneContainer from './components/SceneContainer/SceneContainer'
 
 const App = () => (
-  <Canvas camera={{ position: [0, 0, 2] }}>
-    <SceneContainer />
-  </Canvas>
+  <section>
+    <nav>
+      <a href="/html/">HTML</a> |<a href="/css/">CSS</a> |
+      <a href="/js/">JavaScript</a> |<a href="/python/">Python</a>
+    </nav>
+    <section style={{ height: '100vh' }}>
+      <Canvas camera={{ position: [0, 0, 2] }}>
+        <SceneContainer />
+      </Canvas>{' '}
+    </section>
+    <section>
+      Why do we use it? It is a long established fact that a reader will be
+      distracted by the readable content of a page when looking at its layout.
+      The point of using Lorem Ipsum is that it has a more-or-less normal
+      distribution of letters, as opposed to using 'Content here, content here',
+      making it look like readable English. Many desktop publishing packages and
+      web page editors now use Lorem Ipsum as their default model text, and a
+      search for 'lorem ipsum' will uncover many web sites still in their
+      infancy. Various versions have evolved over the years, sometimes by
+      accident, sometimes on purpose (injected humour and the like).
+    </section>
+  </section>
 )
 
 export default App
--- src/components/SceneContainer/SceneContainer.jsx
+++ src/components/SceneContainer/SceneContainer.jsx
@@ -12,6 +12,29 @@ import FloatingRocks from '../Geometries/FloatingRocks'
 import Trees from '../Geometries/Tree'
 import Words from '../Geometries/Words'
 import Grass from '../Geometries/Grass'
+import {
+  BrightnessContrast,
+  ChromaticAberration,
+  DepthOfField,
+  EffectComposer,
+  GodRays,
+  HueSaturation
+} from '@react-three/postprocessing'
+import { Color, CylinderGeometry, Mesh, MeshBasicMaterial } from 'three'
+import SceneParticles from '../Geometries/SceneParticles'
+
+let lightColor = new Color(1, 0.2, 0.1)
+let mesh = new Mesh(
+  new CylinderGeometry(0.3, 0.3, 0.2, 20),
+  new MeshBasicMaterial({
+    color: lightColor,
+    transparent: true,
+    opacity: 1
+  })
+)
+mesh.rotation.x = Math.PI * 0.5
+mesh.position.set(1.17, 10.7, -4.1)
+mesh.scale.set(1.5, 1, 1)
 
 const SceneContainer = () => {
   return (
@@ -19,6 +42,18 @@ const SceneContainer = () => {
       <Environment background={'only'} files={'../../images/bg.hdr'} />
       <Environment background={false} files={'../../images/envmap.hdr'} />
       <Float speed={1} rotationIntensity={0.35} floatIntensity={0.5}>
+        <primitive object={mesh} />
+        <spotLight
+          penumbra={1}
+          distance={500}
+          angle={60.65}
+          attenuation={1}
+          anglePower={3}
+          intensity={0.3}
+          color={lightColor}
+          position={[1.19, 10.85, -4.45]}
+          target-position={[0, 0, -1]}
+        />
         <FloatingIsland />
         <Rocks />
         <Portal />
@@ -27,12 +62,37 @@ const SceneContainer = () => {
         <Grass />
       </Float>
       <FloatingRocks />
-      <PerspectiveCamera
-        makeDefault
-        fov={50}
-        position={[-1.75, 10.85, 20.35]}
-      />
-      <OrbitControls maxPolarAngle={Math.PI * 0.5} target={[1, 5, 0]} />
+      <PerspectiveCamera makeDefault fov={50} position={[-1, 10.85, 20]} />
+      <SceneParticles />
+      <EffectComposer stencilBuffer={true}>
+        <DepthOfField
+          focusDistance={0.012}
+          focalLength={0.015}
+          bokehScale={7}
+        />
+        <HueSaturation hue={0} saturation={-0.15} />
+        <BrightnessContrast brightness={0.0} contrast={0.035} />
+        <ChromaticAberration
+          radialModulation={true}
+          offset={[0.00175, 0.00175]}
+        />
+        <GodRays
+          sun={mesh}
+          // blendFunction={BlendFunction.Screen}
+          samples={40}
+          density={0.97}
+          decay={0.97}
+          weight={0.6}
+          exposure={0.3}
+          clampMax={1}
+          // width={Resizer.AUTO_SIZE}
+          // height={Resizer.AUTO_SIZE}
+          // kernelSize={KernelSize.SMALL}
+          blur={true}
+        />
+      </EffectComposer>
+
+      <OrbitControls maxPolarAngle={Math.PI * 0.5} target={[1, 4, 0]} />
     </Suspense>
   )
 }
